492cfc14741bafa6d078d03e4af90dd49c6fb7fb
Aantonberkentod <81915887+Aantonberkentod@users.noreply.github.com>
2021-07-04 09:58:10 +0700

    Create mem-tr.sh

---
+++ mem-tr.sh
@@ -0,0 +1,6 @@
+echo -e "-----------------------------------"
+echo -e "User              Masa Aktif Akun |"
+echo -e "-----------------------------------"                                                         
+cat /etc/trojan/akun.conf
+echo -e "================================="
+echo -e "Script By Haweng"
