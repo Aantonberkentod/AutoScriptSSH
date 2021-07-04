268bfde0b0682a4a66e99d89d42fae4c8527abde
Aantonberkentod <81915887+Aantonberkentod@users.noreply.github.com>
2021-07-04 07:34:17 +0700

    Update ins-vt.sh

--- ins-vt.sh
+++ ins-vt.sh
@@ -392,52 +392,52 @@ cat> /etc/v2ray/vnone.json << END
 END
 mkdir /etc/trojan
 cat> /etc/trojan/config.json <<END
-{
-    "run_type": "server"
+    "run_type": "server",
     "local_addr": "0.0.0.0",
     "local_port": 2087,
     "remote_addr": "127.0.0.1",
     "remote_port": 2603,
-    "password": ["$uuid"],
+    "password": [
+        "password1"
+    ],
     "log_level": 1,
     "ssl": {
-         "cert": "/etc/v2ray/v2ray.crt",
-         "key": "/etc/v2ray/v2ray.key",
-         "key_password": "",
-         "cipher": "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384",
-         "cipher_tls13": "TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384",
-         "prefer_server_cipher": true,
-         "alpn": [
-                "http/1.1"
-          ],
-          "reuse_session": true,
-          "session_ticket": false,
-          "session_timeout": 600,
-          "plain_http_response": "",
-          "curves":"",
-          "dhparam": ""
+        "cert": "/etc/v2ray/v2ray.crt",
+        "key": "/etc/v2ray/v2ray.key",
+        "key_password": "",
+        "cipher": "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384",
+        "cipher_tls13": "TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384",
+        "prefer_server_cipher": true,
+        "alpn": [
+            "http/1.1"
+        ],
+
+        "reuse_session": true,
+        "session_ticket": false,
+        "session_timeout": 600,
+        "plain_http_response": "",
+        "curves": "",
+        "dhparam": ""
     },
     "tcp": {
-           "prefer_ipv4": false,
-           "no_delay": true,
-           "keep_alive": true,
-           "reuse_port": false,
-           "fast_open": false,
-           "fast_open_qlen": 20
-    },           
+        "prefer_ipv4": false,
+        "no_delay": true,
+        "keep_alive": true,
+        "reuse_port": false,
+        "fast_open": false,
+        "fast_open_qlen": 20
+    },
     "mysql": {
-           "enabled": false,                                                                                     
-           "server_addr": "127.0.0.1",
-           "server_port": 3306,
-           "database": "trojan",
-           "username": "trojan",
-           "password": "",                                                                                       
-           "key": "",
-           "cert": "",                                                                                           
-           "ca": ""
-                                                                                                
-    }
-}        
+        "enabled": false,
+        "server_addr": "127.0.0.1",
+        "server_port": 3306,
+        "database": "trojan",
+        "username": "trojan",
+        "password": "",
+        "key": "",
+        "cert": "",
+        "ca": ""
+    }        
 END
 iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 8443 -j ACCEPT
 iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 2083 -j ACCEPT
