d49cff21cae15e95c57f20aa6d3db7646def0444
Aantonberkentod <81915887+Aantonberkentod@users.noreply.github.com>
2021-07-22 08:37:37 +0700

    Update websocket.sh

--- websocket.sh
+++ websocket.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-cleae
+clear
 echo Installing Websocket-SSH AUTOSCRIPT BY HABLESSH
-sleep 1
+sleep 2
 echo Cek Hak Akses...
 sleep 0.5
 cd
 #Install system auto run
 wget -O /etc/systemd/system/ws-ssh.service https://raw.githubusercontent.com/Aantonberkentod/AutoScriptSSH/main/ws-ssh.service && chmod +x /etc/systemd/system/ws-ssh.service
-wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/Aantonberkentod/AutoScriptSSH/834e18746c6a3d822aef0a65cc9f3ec470bb6e52/ws-ovpn.service && chmod +x /etc/systemd/system/ws-ovpn.service
+wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/Aantonberkentod/AutoScriptSSH/main/ws-ovpn.service && chmod +x /etc/systemd/system/ws-ovpn.service
 #Install Websocket-SSH
 
 wget -O /usr/local/bin/ws-ssh https://raw.githubusercontent.com/Aantonberkentod/AutoScriptSSH/main/ws-ssh && chmod +x /usr/local/bin/ws-ssh
